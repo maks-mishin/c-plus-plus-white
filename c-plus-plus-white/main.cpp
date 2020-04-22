9ed05f9a1434d8a3a9b94ab429ecff3601fcccd5
maks-mishin <maks.mishinFZ@gmail.com>
2020-04-22 15:36:40 +0300

    -

--- c-plus-plus-white/main.cpp
+++ c-plus-plus-white/main.cpp
@@ -6,8 +6,25 @@
 
 using namespace std;
 
+class Person {
+public:
+	void ChangeFirstName(int year, const string& first_name) {
+		// добавить факт изменения имени на first_name в год year
+	}
+	void ChangeLastName(int year, const string& last_name) {
+		// добавить факт изменения фамилии на last_name в год year
+	}
+	string GetFullName(int year) {
+		// получить имя и фамилию по состоянию на конец года year
+	}
+private:
+	string first_name;
+	string last_name;
+};
+
+
 int main()
 {
-	
+		
 	return 0;
 }
